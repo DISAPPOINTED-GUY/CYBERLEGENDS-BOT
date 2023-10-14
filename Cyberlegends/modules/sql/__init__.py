47547f84d9ecccc1b3a7eb23e6cfac11a19b099e
Rishisup3ryo <117019636+DISAPPOINTED-GUY@users.noreply.github.com>
2023-10-14 12:55:09 +0530

    Update __init__.py

--- Cyberlegends/modules/sql/__init__.py
+++ Cyberlegends/modules/sql/__init__.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
+from sqlalchemy.orm import sessionmaker, scoped_session
 from Cyberlegends import DB_URI
-from Cyberlegends import LOGGER as log
 
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
@@ -11,17 +9,10 @@ if DB_URI and DB_URI.startswith("postgres://"):
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
-    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-try:
-    SESSION = start()
-except Exception as e:
-    log.exception(f"[PostgreSQL] Failed to connect due to {e}")
-    exit()
-
-log.info("[PostgreSQL] Connection successful, session started.")
+SESSION = start()
